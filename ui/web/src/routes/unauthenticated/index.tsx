62c3f252305a77e8106b7b6ef6c6238cabe7ace2
Vinícius Alves <viniciusfariaresende@gmail.com>
2023-08-23 07:46:56 -0300

    feat: added new CreateRoom routing

--- ui/web/src/routes/unauthenticated/index.tsx
+++ ui/web/src/routes/unauthenticated/index.tsx
@@ -5,11 +5,13 @@ import { Route } from 'react-router-dom';
 import UnauthenticatedLayout from '../../components/Layout/Unauthenticated';
 
 /** Route elements */
+import RouteCreateRoom from './RouteCreateRoom';
 import RouteEnterRoom from './RouteEnterRoom';
 
 /** Exports */
 export default (
   <Route path="/" element={<UnauthenticatedLayout />}>
     <Route index element={<RouteEnterRoom />} />
+    <Route path="/rooms/new" element={<RouteCreateRoom />} />
   </Route>
 );

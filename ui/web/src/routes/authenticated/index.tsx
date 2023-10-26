f1f00473ce90908a2ec5f0d4903057454d8b90a5
Vinicius Faria <viniciusfariaresende@gmail.com>
2023-10-26 07:24:39 -0300

    feat: renaming UI layout and route components

--- ui/web/src/routes/authenticated/index.tsx
+++ ui/web/src/routes/authenticated/index.tsx
@@ -2,14 +2,14 @@
 import { Route } from 'react-router-dom';
 
 /** Layouts */
-import AuthenticatedLayout from '../../components/Layout/Authenticated';
+import InRoomLayout from '../../components/Layout/InRoom';
 
 /** Route elements */
 import RouteManageRoom from './RouteManageRoom';
 
 /** Exports */
 export default (
-  <Route path="/rooms/:roomId" element={<AuthenticatedLayout />}>
+  <Route path="/rooms/:roomId" element={<InRoomLayout />}>
     <Route path="/rooms/:roomId/admin" element={<RouteManageRoom />} />
   </Route>
 );

e8176d35c0f16c8fcee6e1f13df0e9a973e8dae3
Vinicius Faria <viniciusfariaresende@gmail.com>
2023-10-29 20:14:35 -0300

    feat: implemented enums that are used to deal with RoomAccess

---
+++ shared/lib/src/data/enums/RoomEnums.ts
@@ -0,0 +1,13 @@
+/**
+ * @category Enum
+ * @module RoomEnums
+ */
+
+/**
+ * Enumerated values for Room action types.
+ */
+export enum RoomActionTypeEnum {
+  ROOM = 'room',
+  QUESTION = 'question',
+  LIKE = 'like',
+}

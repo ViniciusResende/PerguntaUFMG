93f672dd8ee9a2a847876620c89712a6b1c1cb53
Vinicius Faria <viniciusfariaresende@gmail.com>
2023-10-31 06:27:50 -0300

    feat: exporting newly created types from the Lib

--- shared/lib/src/types.ts
+++ shared/lib/src/types.ts
@@ -7,7 +7,11 @@
 export * from './data/types/CommonTypes';
 
 /** Enums */
+export * from './data/enums/CommonEnums';
+export * from './utils/services/notification/NotificationEnums';
 
 /** Interfaces  */
 export * from './data/interfaces/CommonInterfaces';
+export * from './data/interfaces/RoomInterfaces';
 export * from './data/interfaces/UserInterfaces';
+export * from './utils/services/notification/NotificationInterfaces';
